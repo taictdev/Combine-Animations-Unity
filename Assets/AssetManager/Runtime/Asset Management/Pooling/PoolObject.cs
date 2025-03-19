8a63e48e96795d4e312bede26bbf3c48b65f630b
taictdev <taictdev@gmail.com>
2025-03-19 17:23:23 +0700

    update version 2023.3.0b7

--- Assets/AssetManager/Runtime/Asset Management/Pooling/PoolObject.cs
+++ Assets/AssetManager/Runtime/Asset Management/Pooling/PoolObject.cs
@@ -171,7 +171,7 @@ namespace Skywatch.AssetManagement.Pooling
             if (_rigidbody)
             {
                 _rigidbody.isKinematic = true;
-                _rigidbody.velocity = Vector3.zero;
+                _rigidbody.linearVelocity = Vector3.zero;
                 _rigidbody.angularVelocity = Vector3.zero;
                 _rigidbody.rotation = Quaternion.identity;
                 _rigidbody.freezeRotation = true;
